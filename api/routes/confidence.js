9da71548751b717f6fce9c1c61a90b804ee498e4
Timnit Yemane <timnityemane@Timnits-MBP.attlocal.net>
2023-01-02 12:50:39 -0500

    add request header and modified filter

--- api/routes/confidence.js
+++ api/routes/confidence.js
@@ -6,15 +6,18 @@ const https = require('https');
 router.get('/', (req, res, next) => {
     const query = req.query;
     getData(query).then(data => {
+
         const formattedData = states.features.map(state => {
             let existingState = JSON.parse(data).filter(cdcState => cdcState.geography === state.properties.name);
-            if (existingState.length > 0) state.properties['avg_estimate'] = +existingState[0].avg_estimate;
+            if (existingState.length > 0) { state.properties['avg_estimate'] = +existingState[0].avg_estimate;} else { delete state.properties['avg_estimate']}
             return state;
         }).filter(state2 => !!state2.properties.avg_estimate);
+
         res.status(200).json({
             message: "Handling GET request for /confidence",
             query: query,
-            data: formattedData
+            total: formattedData.length,
+            data: formattedData,
         });
     });
     
@@ -23,12 +26,16 @@ router.get('/', (req, res, next) => {
 const getData = (query) => {
 
     return new Promise((resolve, reject) => {
-        const url = `https://data.cdc.gov/resource/udsf-9v7b.json?group_category=${query.group_category && query.group_category}&$select=geography,avg(estimate)&$having=avg_estimate >= 0&$group=geography`;
-        let data = '';
-        https.get(url, (res) => {
+        const url = `https://data.cdc.gov/resource/udsf-9v7b.json?${query && query.group_category ? 'group_category='+query.group_category+'&' : ''}$select=geography,avg(estimate)&$having=avg_estimate >= 0&$group=geography`;
+        const header = {
+            "X-App-Token": "axNYo6uT94yHoUc5cUl0QlhW5"
+        }
+        let data = {};
+
+        https.get(url, {headers: header}, (res) => {
             
             res.on('data', (chunk) => {
-                data += chunk;
+                data = chunk;
                 resolve(data);
                 
             })
